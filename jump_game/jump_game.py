a134f92c039a9fc0c193aa2f41cd0b6e901441a8
Brandon Wong <brandon@yerdle.com>
2020-05-31 15:55:37 -0700

    Add a bottom up solution to jump game

--- jump_game/jump_game.py
+++ jump_game/jump_game.py
@@ -8,3 +8,16 @@ class Solution:
                 last_pos = i
 
         return last_pos == 0
+
+    def bottom_up(self, nums: List[int]) -> bool:
+        dp = [None for _ in range(len(nums))]
+        dp[len(nums) - 1] = True
+
+        for i in reversed(range(len(nums) - 1)):
+            furthest_jump = min(i + nums[i], len(nums) - 1)
+            for j in range(i + 1, furthest_jump + 1):
+                if dp[j]:
+                    dp[i] = True
+                    break
+
+        return dp[0] == True

53e9897dd67ed61abab1b7c2f2e65fc869c69239
Brandon Wong <brandon@yerdle.com>
2020-06-06 10:20:10 -0700

    Implement longest substring with k distinct

--- grokking/sliding_window/longest_substring_with_k_distinct/main.py
+++ grokking/sliding_window/longest_substring_with_k_distinct/main.py
@@ -7,11 +7,13 @@ Example 1:
 Input: String="araaci", K=2
 Output: 4
 Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
 Example 2:
 
 Input: String="araaci", K=1
 Output: 2
 Explanation: The longest substring with no more than '1' distinct characters is "aa".
+
 Example 3:
 
 Input: String="cbbebi", K=3
@@ -20,5 +22,24 @@ Explanation: The longest substrings with no more than '3' distinct characters ar
 """
 
 def longest_substring_with_k_distinct(str, k):
-  # TODO: Write your code here
-  return -1
+    ans = 0
+    start = 0
+    tracker = {}
+    for end in range(len(str)):
+        i = str[end]
+        if i not in tracker:
+            tracker[i] = 0
+        tracker[i] += 1
+
+
+        while len(tracker) > k:
+            first = str[start]
+            tracker[first] -= 1
+            start += 1
+            if tracker[first] == 0:
+                del tracker[first]
+
+
+        ans = max(ans, end - start + 1)
+
+    return ans

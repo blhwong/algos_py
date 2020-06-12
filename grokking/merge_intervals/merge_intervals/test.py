fe460edc981201b85198ac301da97c51a65ab7ec
Brandon Wong <brandon@yerdle.com>
2020-06-11 23:04:50 -0700

    Update merge interval tests

--- grokking/merge_intervals/merge_intervals/test.py
+++ grokking/merge_intervals/merge_intervals/test.py
@@ -1,37 +1,28 @@
 from unittest import TestCase, main
-from grokking.fast_slow_pointers.middle_of_linked_list.main import find_middle_of_linked_list
-from data_structures.list_node import ListNode as Node
+from grokking.merge_intervals.merge_intervals.main import merge
+from data_structures.interval import Interval
 
 
 class TestSuite(TestCase):
     def test_1(self):
-        head = Node(1)
-        head.next = Node(2)
-        head.next.next = Node(3)
-        head.next.next.next = Node(4)
-        head.next.next.next.next = Node(5)
-        self.assertTrue(find_middle_of_linked_list(head) == head.next.next)
+        intervals = [Interval(1, 4), Interval(2, 5), Interval(7, 9)]
+        merged = merge(intervals)
+        expected = [Interval(1, 5), Interval(7, 9)]
+        self.assertTrue(Interval.compare(merged, expected))
+
 
     def test_2(self):
-        head = Node(1)
-        head.next = Node(2)
-        head.next.next = Node(3)
-        head.next.next.next = Node(4)
-        head.next.next.next.next = Node(5)
-        head.next.next.next.next.next = Node(6)
-        self.assertTrue(find_middle_of_linked_list(head)
-                        == head.next.next.next)
+        intervals = [Interval(1, 4), Interval(2, 5), Interval(7, 9)]
+        merged = merge(intervals)
+        expected = [Interval(1, 5), Interval(7, 9)]
+        self.assertTrue(Interval.compare(merged, expected))
+
 
     def test_3(self):
-        head = Node(1)
-        head.next = Node(2)
-        head.next.next = Node(3)
-        head.next.next.next = Node(4)
-        head.next.next.next.next = Node(5)
-        head.next.next.next.next.next = Node(6)
-        head.next.next.next.next.next.next = Node(7)
-        self.assertTrue(find_middle_of_linked_list(head)
-                        == head.next.next.next)
+        intervals = [Interval(1, 4), Interval(2, 5), Interval(7, 9)]
+        merged = merge(intervals)
+        expected = [Interval(1, 5), Interval(7, 9)]
+        self.assertTrue(Interval.compare(merged, expected))
 
 
 if __name__ == '__main__':

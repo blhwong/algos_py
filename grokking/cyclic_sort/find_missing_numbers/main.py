2564040503d479fd0dd58d0afbd17376d32aafde
Brandon Wong <brandon@yerdle.com>
2020-06-13 13:01:44 -0700

    Implement finding all missing numbers

---
+++ grokking/cyclic_sort/find_missing_numbers/main.py
@@ -0,0 +1,34 @@
+"""
+Problem Statement #
+We are given an unsorted array containing numbers taken from the range 1 to ‘n’. The array can have duplicates, which means some numbers will be missing. Find all those missing numbers.
+
+Example 1:
+
+Input: [2, 3, 1, 8, 2, 3, 5, 1]
+Output: 4, 6, 7
+Explanation: The array should have all numbers from 1 to 8, due to duplicates 4, 6, and 7 are missing.
+Example 2:
+
+Input: [2, 4, 1, 2]
+Output: 3
+Example 3:
+
+Input: [2, 3, 2, 1]
+Output: 4
+"""
+
+def find_missing_numbers(nums):
+    missing_numbers = []
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+
+    for idx, val in enumerate(nums):
+        if val != idx + 1:
+            missing_numbers.append(idx + 1)
+
+    return missing_numbers

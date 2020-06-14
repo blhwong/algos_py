d0f7b6b3b7ad59a8962179185e409c69fb5cca89
Brandon Wong <brandon@yerdle.com>
2020-06-14 11:32:14 -0700

    Implement level order traversal

--- grokking/tree_bfs/level_order_traversal/main.py
+++ grokking/tree_bfs/level_order_traversal/main.py
@@ -3,7 +3,24 @@ Problem Statement #
 Given a binary tree, populate an array to represent its level-by-level traversal. You should populate the values of all nodes of each level from left to right in separate sub-arrays.
 """
 
+from collections import deque
+
 def traverse(root):
     result = []
-    # TODO: Write your code here
+    q = deque()
+    q.append(root)
+
+    while q:
+        curr_level = []
+        curr_len = len(q)
+        for _ in range(curr_len):
+            curr = q.popleft()
+            curr_level.append(curr.val)
+            if curr.left:
+                q.append(curr.left)
+            if curr.right:
+                q.append(curr.right)
+
+        result.append(curr_level)
+
     return result
--- grokking/tree_bfs/level_order_traversal/test.py
+++ grokking/tree_bfs/level_order_traversal/test.py
@@ -11,6 +11,22 @@ class TestSuite(TestCase):
         root.left.left = TreeNode(9)
         root.right.left = TreeNode(10)
         root.right.right = TreeNode(5)
+        result = traverse(root)
+        expected = [[12], [7, 1], [9, 10, 5]]
+        self.assertListEqual(result, expected)
+
+
+    def test_2(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+        root.right.left = TreeNode(6)
+        root.right.right = TreeNode(7)
+        result = traverse(root)
+        expected = [[1], [2, 3], [4, 5, 6, 7]]
+        self.assertListEqual(result, expected)
 
 
 if __name__ == '__main__':

b48d1302dec1a6ef11e43239d207c4d231640faa
Brandon Wong <brandon@yerdle.com>
2020-07-07 18:25:16 -0500

    Implement min height bst

--- algo_exp/min_height_bst/main.py
+++ algo_exp/min_height_bst/main.py
@@ -1,2 +1,21 @@
+from data_structures.binary_search_tree.main import BST
+from collections import deque
+
 def minHeightBst(array):
-    pass
+    ans = None
+    q = deque()
+    left, right = 0, len(array) - 1
+    q.append((left, right))
+    while q:
+        left, right = q.popleft()
+        if right < left:
+            continue
+        mid = (left + right) // 2
+        if ans:
+            ans.insert(array[mid])
+        else:
+            ans = BST(array[mid])
+        q.append((left, mid - 1))
+        q.append((mid + 1, right))
+
+    return ans

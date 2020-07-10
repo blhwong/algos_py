536c5328ff0aff2bea2cf8cd3b020ac235425aa0
Brandon Wong <brandon@yerdle.com>
2020-07-10 14:45:47 -0500

    Simplify fib function

--- algo_exp/nth_fibonacci/main.py
+++ algo_exp/nth_fibonacci/main.py
@@ -2,9 +2,9 @@ def getNthFib(n):
     i, prev, curr = 1, 0, 1
 
     while i < n:
-        tmp = curr
-        curr = prev + curr
-        prev = tmp
+        tmp = prev
+        prev = curr
+        curr += tmp
         i += 1
 
     return prev

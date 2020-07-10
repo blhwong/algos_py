32a94efc329fe8e5502d6110fc5d4a6a9299a629
Brandon Wong <brandon@yerdle.com>
2020-07-10 14:11:55 -0500

    Reimplement branch sums

--- algo_exp/branch_sums/main.py
+++ algo_exp/branch_sums/main.py
@@ -1,14 +1,18 @@
 def branchSums(root):
     ans = []
     def traverse(curr, curr_sum):
+        if not curr:
+            return
+
         new_sum = curr_sum + curr.value
+
         if not curr.left and not curr.right:
             ans.append(new_sum)
-            return
-        if curr.left:
-            traverse(curr.left, new_sum)
-        if curr.right:
-            traverse(curr.right, new_sum)
+
+        traverse(curr.left, new_sum)
+        traverse(curr.right, new_sum)
+
 
     traverse(root, 0)
+
     return ans

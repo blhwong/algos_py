bf687747ac716bdf0489cc4812da35b8748feeaa
Brandon Wong <brandon@yerdle.com>
2020-07-12 14:37:49 -0500

    Cleanup

--- algo_exp/min_heap_construction/test.py
+++ algo_exp/min_heap_construction/test.py
@@ -1,14 +1,6 @@
 from unittest import TestCase, main
 from algo_exp.min_heap_construction.main import MinHeap
 
-
-def isMinHeapPropertySatisfied(array):
-    for currentIdx in range(1, len(array)):
-        parentIdx = (currentIdx - 1) // 2
-        if array[parentIdx] > array[currentIdx]:
-            return False
-    return True
-
 class TestSuite(TestCase):
     def test_1(self):
         array = [48, 12, 24, 7, 8, -5, 24, 391, 24, 56, 2, 6, 8, 41]

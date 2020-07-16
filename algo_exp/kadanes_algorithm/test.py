8806fd0e4974700ee3aab211df310dba5a85d184
Brandon Wong <brandon@yerdle.com>
2020-07-16 16:53:23 -0700

    Add extra tests for kadanes algorithm

--- algo_exp/kadanes_algorithm/test.py
+++ algo_exp/kadanes_algorithm/test.py
@@ -9,6 +9,12 @@ class TestSuite(TestCase):
     def test_2(self):
         self.assertEqual(kadanesAlgorithm([-1, -2, -3]), -1)
 
+    def test_3(self):
+        self.assertEqual(kadanesAlgorithm([]), 0)
+
+    def test_4(self):
+        self.assertEqual(kadanesAlgorithm([-2]), -2)
+
 
 if __name__ == '__main__':
     main()

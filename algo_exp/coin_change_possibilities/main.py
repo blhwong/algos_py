b9f0593c88eaa173592f84c6b1f5ff7f979b1c60
Brandon Wong <brandon@yerdle.com>
2020-07-19 14:12:05 -0700

    Cleanup coin change possibilities

--- algo_exp/coin_change_possibilities/main.py
+++ algo_exp/coin_change_possibilities/main.py
@@ -1,3 +1,10 @@
+"""
+   0  1  2  3  4  5  6  7  8  9  10
+1  1  1  1  1  1  1  1  1  1  1  1
+5  1  1  1  1  1  2  2  2  2  2  3
+10 1  1  1  1  1  2  2  2  2  2  4
+"""
+
 def numberOfWaysToMakeChange(n, denoms):
     if not denoms:
         return 0
@@ -6,6 +13,6 @@ def numberOfWaysToMakeChange(n, denoms):
     dp[0] = 1
     for coin in denoms:
         for i in range(coin, n + 1):
-                dp[i] += dp[i - coin]
+            dp[i] += dp[i - coin]
 
     return dp[-1]

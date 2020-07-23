c7c91da4a0c4e394001f16ed438a40982afb08a6
Brandon Wong <brandon@yerdle.com>
2020-07-23 14:59:13 -0700

    Fix solution

--- leet/min_integer_after_k_swaps/main.py
+++ leet/min_integer_after_k_swaps/main.py
@@ -1,21 +1,12 @@
 class Solution:
     def minInteger(self, num: str, k: int) -> str:
-        arr = list(num)
+        if k <= 0:
+            return num
+        n = len(num)
+        if k >= n*(n-1)//2:
+            return "".join(sorted(list(num)))
 
-        swaps_left = k
-
-        for i in range(len(arr)):
-            if not swaps_left:
-                break
-            min_idx = i
-            for j in range(i + 1, min(len(arr), i + swaps_left + 1)):
-                if arr[j] < arr[min_idx]:
-                    min_idx = j
-
-            if i != min_idx:
-                swaps_left -= min_idx - i
-                for j in range(min_idx, i, -1):
-                    arr[j], arr[j - 1] = arr[j - 1], arr[j]
-
-
-        return ''.join(arr)
+        for i in range(10):
+            ind = num.find(str(i))
+            if 0 <= ind <= k:
+                return str(num[ind]) + self.minInteger(num[0:ind] + num[ind+1:], k-ind)

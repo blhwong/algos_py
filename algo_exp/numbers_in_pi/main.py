1a0ae977c46f2f68ce261af339c2f682a66a1b3a
Brandon Wong <brandon@yerdle.com>
2020-07-25 14:30:17 -0700

    Use set for quick lookup of prefixes

--- algo_exp/numbers_in_pi/main.py
+++ algo_exp/numbers_in_pi/main.py
@@ -1,5 +1,5 @@
 def numbersInPi(pi, numbers):
-    def solve(start, memo):
+    def solve(start, memo, prefixes):
         if len(pi) == start:
             return -1
 
@@ -7,14 +7,14 @@ def numbersInPi(pi, numbers):
             return memo[start]
 
         ans = float('inf')
-        for num in numbers:
-            if pi[start:].startswith(num):
-                suffix = solve(start + len(num), memo)
+        for i in range(start, len(pi)):
+            prefix = pi[start:i + 1]
+            if prefix in prefixes:
+                suffix = solve(start + len(prefix), memo, prefixes)
                 ans = min(ans, suffix + 1)
 
         memo[start] = ans
         return ans
 
-
-    ans = solve(0, {})
+    ans = solve(0, {}, set(numbers))
     return ans if ans != float('inf') else -1

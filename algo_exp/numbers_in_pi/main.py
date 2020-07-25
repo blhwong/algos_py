6046dc615ebd5dcfdd0a34b736368f1820ff2f8f
Brandon Wong <brandon@yerdle.com>
2020-07-25 14:27:00 -0700

    Optimize numbers in pi

--- algo_exp/numbers_in_pi/main.py
+++ algo_exp/numbers_in_pi/main.py
@@ -1,15 +1,20 @@
 def numbersInPi(pi, numbers):
-    def solve(pi_left, used):
-        if len(pi_left) == 0:
-            return used - 1
+    def solve(start, memo):
+        if len(pi) == start:
+            return -1
+
+        if start in memo:
+            return memo[start]
 
         ans = float('inf')
         for num in numbers:
-            if pi_left.startswith(num):
-                ans = min(ans, solve(pi_left[len(num):], used + 1))
+            if pi[start:].startswith(num):
+                suffix = solve(start + len(num), memo)
+                ans = min(ans, suffix + 1)
 
+        memo[start] = ans
         return ans
 
 
-    ans = solve(pi, 0)
+    ans = solve(0, {})
     return ans if ans != float('inf') else -1

ef409f8c01c615bee276224878cfd9c6b96c7ec3
Brandon Wong <brandon@yerdle.com>
2020-08-01 12:48:01 -0700

    Fix heapsort

--- algo_exp/heapsort/main.py
+++ algo_exp/heapsort/main.py
@@ -1,26 +1,26 @@
 def compare(a, b):
     return a > b
 
-def get_swap_idx(i, heap):
+def get_swap_idx(i, end, heap):
     child1_idx, child2_idx = i * 2 + 1, i * 2 + 2
-    if child2_idx < len(heap) and compare(heap[child2_idx], heap[child1_idx]):
+    if child2_idx < end and compare(heap[child2_idx], heap[child1_idx]):
         return child2_idx
     return child1_idx
 
 def sift_down(start, end, heap):
     curr_idx = start
-    swap_idx = get_swap_idx(curr_idx, heap)
+    swap_idx = get_swap_idx(curr_idx, end, heap)
     while swap_idx < end:
         if not compare(heap[swap_idx], heap[curr_idx]):
             break
         heap[swap_idx], heap[curr_idx] = heap[curr_idx], heap[swap_idx]
         curr_idx = swap_idx
-        swap_idx = get_swap_idx(curr_idx, heap)
+        swap_idx = get_swap_idx(curr_idx, end, heap)
 
 def heapify(array):
     first_parent_idx = (len(array) - 2) // 2
     for i in reversed(range(first_parent_idx + 1)):
-        sift_down(i, len(array) - 1, array)
+        sift_down(i, len(array), array)
 
 def heapSort(array):
     heapify(array)
--- algo_exp/heapsort/test.py
+++ algo_exp/heapsort/test.py
@@ -13,6 +13,13 @@ class TestSuite(TestCase):
         self.assertListEqual(heapSort(array), [
                              1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
 
+    def test_3(self):
+        array = [8, 5, 2, 9, 5, 6, 3]
+        self.assertListEqual(heapSort(array), [2, 3, 5, 5, 6, 8, 9])
+
+    def test_4(self):
+        array = [1, 2]
+        self.assertListEqual(heapSort(array), [1, 2])
 
 if __name__ == '__main__':
     main()

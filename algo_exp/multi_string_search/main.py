a89e7d83c19cc8d9ba9afbe76fa35663ed8623e6
Brandon Wong <brandon@yerdle.com>
2020-08-08 14:41:36 -0700

    Implement multi string search

---
+++ algo_exp/multi_string_search/main.py
@@ -0,0 +1,38 @@
+class Trie:
+    def __init__(self, strings):
+        self.end_symbol = '*'
+        self.root = self.build_trie(strings)
+
+    def build_trie(self, strings):
+        root = {}
+        for string in strings:
+            curr = root
+            for char in string:
+                if char not in curr:
+                    curr[char] = {}
+                curr = curr[char]
+            curr[self.end_symbol] = True
+        return root
+
+def search_trie(t, string):
+    curr = t.root
+    found = True
+    for char in string:
+        if char not in curr:
+            found = False
+            break
+        curr = curr[char]
+
+    return found
+
+def multiStringSearch(bigString, smallStrings):
+    bigStrings = bigString.split(' ')
+    prefix_trie = Trie(bigStrings)
+    suffix_trie = Trie([string[::-1] for string in bigStrings])
+    ans = []
+    for string in smallStrings:
+        f1 = search_trie(prefix_trie, string)
+        f2 = search_trie(suffix_trie, string[::-1])
+        ans.append(search_trie(prefix_trie, string) or search_trie(suffix_trie, string[::-1]))
+
+    return ans

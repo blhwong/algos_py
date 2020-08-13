dfadeb61bb6145504e089589be36a9b9eb8eea0e
Brandon Wong <brandon@yerdle.com>
2020-08-13 13:32:46 -0700

    Uncomment tests

--- algo_exp/palindrome_partitioning_min_cuts/test.py
+++ algo_exp/palindrome_partitioning_min_cuts/test.py
@@ -6,14 +6,14 @@ class TestSuite(TestCase):
     def test_1(self):
         self.assertEqual(palindromePartitioningMinCuts('noonabbad'), 2)
 
-    # def test_2(self):
-    #     self.assertEqual(palindromePartitioningMinCuts('a'), 0)
+    def test_2(self):
+        self.assertEqual(palindromePartitioningMinCuts('a'), 0)
 
-    # def test_3(self):
-    #     self.assertEqual(palindromePartitioningMinCuts('abbba'), 0)
+    def test_3(self):
+        self.assertEqual(palindromePartitioningMinCuts('abbba'), 0)
 
-    # def test_4(self):
-    #     self.assertEqual(palindromePartitioningMinCuts('ababbbabbababa'), 3)
+    def test_4(self):
+        self.assertEqual(palindromePartitioningMinCuts('ababbbabbababa'), 3)
 
 
 if __name__ == '__main__':

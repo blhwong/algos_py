e29ff5f853e05dc5a390492feaabf72813ab0c35
Brandon Wong <brandon@yerdle.com>
2020-08-14 19:17:13 -0700

    Memoize string chains

--- algo_exp/longest_string_chain/main.py
+++ algo_exp/longest_string_chain/main.py
@@ -1,27 +1,31 @@
 def by_length(x):
     return len(x)
 
-def get_chains(curr, tracker, curr_chain):
+def get_chains(curr, tracker, curr_chain, memo):
     if curr not in tracker:
         return curr_chain
+    if curr in memo:
+        return memo[curr]
 
     curr_chain.append(curr)
 
     sub_chain = []
     for i in range(len(curr)):
         new = curr[0:i] + curr[i + 1:]
-        sub_chain = max(sub_chain, get_chains(new, tracker, []), key=by_length)
+        sub_chain = max(sub_chain, get_chains(new, tracker, [], memo), key=by_length)
 
-    return curr_chain + sub_chain
+    memo[curr] = curr_chain + sub_chain
+    return memo[curr]
 
 def longestStringChain(strings):
     ans = []
     tracker = set(strings)
+    memo = {}
 
     strings.sort(key=by_length, reverse=True)
 
     for string in strings:
-        chain = get_chains(string, tracker, [])
+        chain = get_chains(string, tracker, [], memo)
         ans = max(ans, chain, key=by_length)
 
     return ans if len(ans) > 1 else []

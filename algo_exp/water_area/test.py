8ff8dd59e87bae66cfb57c4d6f21b74206260929
Brandon Wong <brandon@yerdle.com>
2020-08-16 12:47:24 -0700

    Fix tests

--- algo_exp/water_area/test.py
+++ algo_exp/water_area/test.py
@@ -1,10 +1,5 @@
-from unittest import TestCase, main
 from algo_exp.water_area.main import waterArea
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        self.assertEqual(waterArea([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3]), 48)
-
-if __name__ == '__main__':
-    main()
+def test_1():
+    assert waterArea([0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3]) == 48

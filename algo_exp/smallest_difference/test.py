9a96ba5517cba99a7f37d04f4303a7b125240d75
Brandon Wong <brandon@yerdle.com>
2020-08-19 11:28:23 -0700

    Switch to pytest

--- algo_exp/smallest_difference/test.py
+++ algo_exp/smallest_difference/test.py
@@ -1,11 +1,5 @@
-from unittest import TestCase, main
 from algo_exp.smallest_difference.main import smallestDifference
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        self.assertListEqual(smallestDifference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]), [28, 26])
-
-
-if __name__ == '__main__':
-    main()
+def test_1():
+    assert smallestDifference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]) == [28, 26]

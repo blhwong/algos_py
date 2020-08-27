7b0e175753eb7dbf710d5158743f9e16e151f6a3
Brandon Wong <brandon@yerdle.com>
2020-08-27 14:05:20 -0700

    Refactor

--- algo_exp/longest_substring_without_duplication/main.py
+++ algo_exp/longest_substring_without_duplication/main.py
@@ -2,13 +2,10 @@ def longestSubstringWithoutDuplication(string):
     start = 0
     ans = ''
     tracker = {}
-    for end in range(len(string)):
-        curr = string[end]
-        if curr in tracker:
-            start = max(start, tracker[curr] + 1)
-
-        tracker[curr] = end
-
-        ans = max(ans, string[start:end + 1], key = lambda x: len(x))
+    for end, char in enumerate(string):
+        if char in tracker:
+            start = max(start, tracker[char] + 1)
+        tracker[char] = end
+        ans = max(ans, string[start:end + 1], key=len)
 
     return ans

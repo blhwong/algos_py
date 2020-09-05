94c33d1787af1e61ce9718f8967f4b7b654646f4
Brandon Wong <brandon@yerdle.com>
2020-09-05 18:07:44 -0500

    Revert to original

--- algo_exp/four_number_sum/test.py
+++ algo_exp/four_number_sum/test.py
@@ -1,21 +1,9 @@
-from unittest import TestCase, main
-from algo_exp.four_number_sum.main import fourNumberSum, fourSum
+from algo_exp.four_number_sum.main import four_number_sum
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        self.assertCountEqual(fourNumberSum([7, 6, 4, -1, 1, 2], 16), [
-            [7, 6, 4, -1],
-            [7, 6, 1, 2],
-        ])
-
-    def test_2(self):
-        self.assertCountEqual(fourSum([7, 6, 4, -1, 1, 2], 16), [
-            [-1, 4, 6, 7],
-            [1, 2, 6, 7],
-        ])
-
-
-
-if __name__ == '__main__':
-    main()
+def test_1():
+    expected = [
+        [-1, 4, 6, 7],
+        [1, 2, 6, 7],
+    ]
+    assert four_number_sum([7, 6, 4, -1, 1, 2], 16) == expected

4bf76feca1776e3a0aba09b658c401f449435c8a
Brandon Wong <brandon@yerdle.com>
2020-09-05 17:45:57 -0500

    Revert to original

--- algo_exp/two_sum/main.py
+++ algo_exp/two_sum/main.py
@@ -1,12 +1,2 @@
-def twoNumberSum(array, targetSum):
-    tracker = set()
-
-    for num in array:
-        diff = targetSum - num
-
-        if diff in tracker:
-            return [num, diff]
-        else:
-            tracker.add(num)
-
-    return []
+def two_number_sum(array, target_sum):
+    pass
--- algo_exp/two_sum/test.py
+++ algo_exp/two_sum/test.py
@@ -1,10 +1,10 @@
 from unittest import TestCase, main
-from algo_exp.two_sum.main import twoNumberSum
+from algo_exp.two_sum.main import two_number_sum
 
 
 class TestSuite(TestCase):
     def test_1(self):
-        self.assertCountEqual(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10), [-1, 11])
+        self.assertCountEqual(two_number_sum([3, 5, -4, 8, 11, 1, -1, 6], 10), [-1, 11])
 
 
 if __name__ == '__main__':

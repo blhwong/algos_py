9476cffc3ecc4ad1b762262b63061b29d60ea5c6
Brandon Wong <brandon@yerdle.com>
2020-09-05 17:52:00 -0500

    Revert to original

--- algo_exp/three_sum/test.py
+++ algo_exp/three_sum/test.py
@@ -1,15 +1,10 @@
-from unittest import TestCase, main
-from algo_exp.three_sum.main import threeNumberSum
+from algo_exp.three_sum.main import three_number_sum
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        self.assertListEqual(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0), [
-            [-8, 2, 6],
-            [-8, 3, 5],
-            [-6, 1, 5]
-        ])
-
-
-if __name__ == '__main__':
-    main()
+def test_1():
+    expected = [
+        [-8, 2, 6],
+        [-8, 3, 5],
+        [-6, 1, 5],
+    ]
+    assert three_number_sum([12, 3, 1, 2, -6, 5, -8, 6], 0) == expected

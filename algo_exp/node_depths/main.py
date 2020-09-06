de1d8bb4749d30ab4b0b7401f137b181a355dddb
Brandon Wong <brandon@yerdle.com>
2020-09-05 19:03:57 -0500

    Revert to original

--- algo_exp/node_depths/main.py
+++ algo_exp/node_depths/main.py
@@ -1,17 +1,2 @@
-from collections import deque
-
-def nodeDepths(root):
-    q = deque()
-    q.append((root, 0))
-
-    ans = 0
-
-    while q:
-        curr, depth = q.popleft()
-        ans += depth
-        if curr.left:
-            q.append((curr.left, depth + 1))
-        if curr.right:
-            q.append((curr.right, depth + 1))
-
-    return ans
+def node_depths(root):
+    pass
--- algo_exp/node_depths/test.py
+++ algo_exp/node_depths/test.py
@@ -1,4 +1,4 @@
-from algo_exp.node_depths.main import nodeDepths
+from algo_exp.node_depths.main import node_depths
 from data_structures.tree_node import TreeNode
 
 
@@ -12,4 +12,4 @@ def test_1():
     root.left.left.right = TreeNode(9)
     root.right.left = TreeNode(6)
     root.right.right = TreeNode(7)
-    assert nodeDepths(root) == 16
+    assert node_depths(root) == 16

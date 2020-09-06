de29135c6405353a9242e1a7d12937084f7dc82e
Brandon Wong <brandon@yerdle.com>
2020-09-06 14:49:01 -0500

    Revert to original

--- algo_exp/monotonic_array/main.py
+++ algo_exp/monotonic_array/main.py
@@ -1,18 +1,2 @@
-def isMonotonic(array):
-    is_increasing = None
-    for i in range(len(array) - 1):
-        j = i + 1
-        num_i = array[i]
-        num_j = array[j]
-        if is_increasing is None:
-            if num_i < num_j:
-                is_increasing = True
-            elif num_i > num_j:
-                is_increasing = False
-        else:
-            if num_i < num_j and not is_increasing:
-                return False
-            elif num_i > num_j and is_increasing:
-                return False
-
-    return True
+def is_monotonic(array):
+    pass
--- algo_exp/monotonic_array/test.py
+++ algo_exp/monotonic_array/test.py
@@ -1,17 +1,17 @@
-from algo_exp.monotonic_array.main import isMonotonic
+from algo_exp.monotonic_array.main import is_monotonic
 
 
 def test_1():
-    assert isMonotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]) is True
+    assert is_monotonic([-1, -5, -10, -1100, -1100, -1101, -1102, -9001]) is True
 
 def test_2():
-    assert isMonotonic([1, 2, 3, 4, 5, 6, 7]) is True
+    assert is_monotonic([1, 2, 3, 4, 5, 6, 7]) is True
 
 def test_3():
-    assert isMonotonic([1, 1, 1, 1, 1, 1, 1]) is True
+    assert is_monotonic([1, 1, 1, 1, 1, 1, 1]) is True
 
 def test_4():
-    assert isMonotonic([1, 0, 1]) is False
+    assert is_monotonic([1, 0, 1]) is False
 
 def test_5():
-    assert isMonotonic([0, 1, 2, 2, 1]) is False
+    assert is_monotonic([0, 1, 2, 2, 1]) is False

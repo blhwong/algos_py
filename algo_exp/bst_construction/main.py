6cc338f627a75aad49bdf7d3c006a6b68fe3665c
Brandon Wong <brandon@yerdle.com>
2020-09-06 15:31:40 -0500

    Revert to original

--- algo_exp/bst_construction/main.py
+++ algo_exp/bst_construction/main.py
@@ -2,8 +2,6 @@
 # the insert, contains, and remove methods.
 # Feel free to add new properties and methods
 # to the class.
-
-
 class BST:
     def __init__(self, value):
         self.value = value
@@ -11,70 +9,15 @@ class BST:
         self.right = None
 
     def insert(self, value):
-        curr = self
-        while curr:
-            if value < curr.value:
-                if curr.left:
-                    curr = curr.left
-                else:
-                    curr.left = BST(value)
-                    break
-            else:
-                if curr.right:
-                    curr = curr.right
-                else:
-                    curr.right = BST(value)
-                    break
-
+        # Write your code here.
+        # Do not edit the return statement of this method.
         return self
 
     def contains(self, value):
-        curr = self
-        while curr:
-            if value == curr.value:
-                return True
-            if value < curr.value:
-                curr = curr.left
-            else:
-                curr = curr.right
+        # Write your code here.
+        pass
 
-        return False
-
-
-    def remove(self, value, parent = None):
-        curr = self
-        while curr:
-            if value < curr.value:
-                parent = curr
-                curr = curr.left
-            elif value > curr.value:
-                parent = curr
-                curr = curr.right
-            else:
-                if curr.left and curr.right:
-                    curr.value = curr.right.get_min()
-                    curr.right.remove(curr.value, curr)
-                elif not parent:
-                    if curr.left:
-                        curr.value = curr.left.value
-                        curr.right = curr.left.right
-                        curr.left = curr.left.left
-                    elif curr.right:
-                        curr.value = curr.right.value
-                        curr.left = curr.right.left
-                        curr.right = curr.right.right
-                    else:
-                        pass
-                elif parent.left == curr:
-                    parent.left = curr.left if curr.left else curr.right
-                elif parent.right == curr:
-                    parent.right = curr.left if curr.left else curr.right
-                break
-
+    def remove(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
         return self
-
-    def get_min(self):
-        curr = self
-        while curr.left:
-            curr = curr.left
-        return curr.value

131dffbd796b12955bd0807921ce794c6bb5a35f
Brandon Wong <brandon@yerdle.com>
2020-09-06 15:25:04 -0500

    Add more test cases

--- algo_exp/spiral_traverse/test.py
+++ algo_exp/spiral_traverse/test.py
@@ -6,7 +6,19 @@ def test_1():
         [1, 2, 3, 4],
         [12, 13, 14, 5],
         [11, 16, 15, 6],
-        [10, 9, 8, 7]
+        [10, 9, 8, 7],
     ])
     expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    assert result == expected
+
+def test_2():
+    assert spiral_traverse([[1]]) == [1]
+
+def test_3():
+    result = spiral_traverse([
+        [1, 2, 3],
+        [8, 9, 4],
+        [7, 6, 5],
+    ])
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert result == expected

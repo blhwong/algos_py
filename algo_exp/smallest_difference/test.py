b067043a706f2247f764c6a46cc7128903eb625f
Brandon Wong <brandon@yerdle.com>
2020-09-06 14:30:26 -0500

    Revert to original

--- algo_exp/smallest_difference/test.py
+++ algo_exp/smallest_difference/test.py
@@ -1,5 +1,5 @@
-from algo_exp.smallest_difference.main import smallestDifference
+from algo_exp.smallest_difference.main import smallest_difference
 
 
 def test_1():
-    assert smallestDifference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]) == [28, 26]
+    assert smallest_difference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]) == [28, 26]

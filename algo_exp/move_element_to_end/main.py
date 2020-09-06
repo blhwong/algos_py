ecc8c2d1c711fed574fc5c061e2895407d2ca751
Brandon Wong <brandon@yerdle.com>
2020-09-06 14:40:07 -0500

    Revert to original

--- algo_exp/move_element_to_end/main.py
+++ algo_exp/move_element_to_end/main.py
@@ -1,15 +1,2 @@
-def moveElementToEnd(array, toMove):
-    left, right = 0, len(array) - 1
-    while left < right:
-        while left < right and array[right] == toMove:
-            right -= 1
-        if array[left] == toMove:
-            array[left], array[right] = array[right], array[left]
-        left += 1
-    return array
-
-"""
-2, 1, 2, 2, 2, 3, 4, 2
-4, 1, 2, 2, 2, 3, 2, 2
-4, 1, 3, 2, 2, 2, 2, 2
-"""
+def move_element_to_end(array, to_move):
+    pass
--- algo_exp/move_element_to_end/test.py
+++ algo_exp/move_element_to_end/test.py
@@ -1,5 +1,5 @@
-from algo_exp.move_element_to_end.main import moveElementToEnd
+from algo_exp.move_element_to_end.main import move_element_to_end
 
 
 def test_1():
-    assert moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2], 2) == [4, 1, 3, 2, 2, 2, 2, 2]
+    assert move_element_to_end([2, 1, 2, 2, 2, 3, 4, 2], 2) == [4, 1, 3, 2, 2, 2, 2, 2]

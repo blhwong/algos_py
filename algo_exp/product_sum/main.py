de710378c31308f6f2fa5f3dd306325ad7d1e9ac
Brandon Wong <brandon@yerdle.com>
2020-09-06 13:12:06 -0500

    Revert to original

--- algo_exp/product_sum/main.py
+++ algo_exp/product_sum/main.py
@@ -1,14 +1,5 @@
 # Tip: You can use the type(element) function to check whether an item
 # is a list or an integer.
 
-def productSum(array):
-    def get_sum(arr, depth):
-        ans = 0
-        for curr in arr:
-            if type(curr) is list:
-                ans += get_sum(curr, depth + 1)
-            else:
-                ans += curr
-        return ans * depth
-
-    return get_sum(array, 1)
+def product_sum(array):
+    pass
--- algo_exp/product_sum/test.py
+++ algo_exp/product_sum/test.py
@@ -1,8 +1,8 @@
-from algo_exp.product_sum.main import productSum
+from algo_exp.product_sum.main import product_sum
 
 
 def test_1():
-    assert productSum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]) == 12
+    assert product_sum([5, 2, [7, -1], 3, [6, [-13, 8], 4]]) == 12
 
 def test_2():
-    assert productSum([1, 2, 3, 4, 5]) == 15
+    assert product_sum([1, 2, 3, 4, 5]) == 15

26d6d818503fdaf4e399ce3bfb8ba06055be1da2
Brandon Wong <brandon@yerdle.com>
2020-09-06 13:25:37 -0500

    Revert to original

--- algo_exp/three_largest_numbers/main.py
+++ algo_exp/three_largest_numbers/main.py
@@ -1,18 +1,2 @@
-from collections import deque
-
-def replace(q, i, num):
-    q.insert(i + 1, num)
-    q.popleft()
-
-def findThreeLargestNumbers(array):
-    ans = deque([-float('inf')] * 3)
-
-    for num in array:
-        if num > ans[2]:
-            replace(ans, 2, num)
-        elif num > ans[1]:
-            replace(ans, 1, num)
-        elif num > ans[0]:
-            replace(ans, 0, num)
-
-    return list(ans)
+def find_three_largest_numbers(array):
+    pass

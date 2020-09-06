5da067a2ce8c87ca22e1b9658850f16ff4b21767
Brandon Wong <brandon@yerdle.com>
2020-09-06 13:24:04 -0500

    Revert to original

--- algo_exp/binary_search/main.py
+++ algo_exp/binary_search/main.py
@@ -1,13 +1,2 @@
-def binarySearch(array, target):
-    left, right = 0, len(array) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
+def binary_search(array, target):
+    pass
--- algo_exp/binary_search/test.py
+++ algo_exp/binary_search/test.py
@@ -1,5 +1,5 @@
-from algo_exp.binary_search.main import binarySearch
+from algo_exp.binary_search.main import binary_search
 
 
 def test_1():
-    assert binarySearch([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33) == 3
+    assert binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33) == 3

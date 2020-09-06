4e217bd5680d238360997648be12a4a7a8a60745
Brandon Wong <brandon@yerdle.com>
2020-09-06 14:05:11 -0500

    Revert to original

--- algo_exp/insertion_sort/main.py
+++ algo_exp/insertion_sort/main.py
@@ -1,8 +1,4 @@
 # For each number in i, go backwards and swap if misplaced until the start of array
 
-def insertionSort(array):
-    for i in range(1, len(array)):
-        for j in range(i, 0, -1):
-            if array[j] < array[j - 1]:
-                array[j], array[j - 1] = array[j - 1], array[j]
-    return array
+def insertion_sort(array):
+    pass
--- algo_exp/insertion_sort/test.py
+++ algo_exp/insertion_sort/test.py
@@ -1,5 +1,5 @@
-from algo_exp.insertion_sort.main import insertionSort
+from algo_exp.insertion_sort.main import insertion_sort
 
 
 def test_1():
-    assert insertionSort([8, 5, 2, 9, 5, 6, 3]) == [2, 3, 5, 5, 6, 8, 9]
+    assert insertion_sort([8, 5, 2, 9, 5, 6, 3]) == [2, 3, 5, 5, 6, 8, 9]

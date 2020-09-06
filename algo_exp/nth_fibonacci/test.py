6a666bc54abd439942160570cc02d0c1252c7d52
Brandon Wong <brandon@yerdle.com>
2020-09-06 13:11:01 -0500

    Add more tests

--- algo_exp/nth_fibonacci/test.py
+++ algo_exp/nth_fibonacci/test.py
@@ -2,7 +2,25 @@ from algo_exp.nth_fibonacci.main import get_nth_fib
 
 
 def test_1():
-    assert get_nth_fib(2) == 1
+    assert get_nth_fib(1) == 0
 
 def test_2():
+    assert get_nth_fib(2) == 1
+
+def test_3():
+    assert get_nth_fib(3) == 1
+
+def test_4():
+    assert get_nth_fib(4) == 2
+
+def test_5():
+    assert get_nth_fib(5) == 3
+
+def test_6():
     assert get_nth_fib(6) == 5
+
+def test_7():
+    assert get_nth_fib(7) == 8
+
+def test_8():
+    assert get_nth_fib(8) == 13

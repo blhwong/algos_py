42334541fc2ab0e586ab826bc29068bc7c3f1de5
Brandon Wong <brandon@yerdle.com>
2020-09-06 15:00:36 -0500

    Revert to original

--- algo_exp/spiral_traverse/test.py
+++ algo_exp/spiral_traverse/test.py
@@ -1,16 +1,12 @@
-from unittest import TestCase, main
-from algo_exp.spiral_traverse.main import spiralTraverse
+from algo_exp.spiral_traverse.main import spiral_traverse
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        self.assertListEqual(spiralTraverse([
-            [1, 2, 3, 4],
-            [12, 13, 14, 5],
-            [11, 16, 15, 6],
-            [10, 9, 8, 7]
-        ]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-
-
-if __name__ == '__main__':
-    main()
+def test_1():
+    result = spiral_traverse([
+        [1, 2, 3, 4],
+        [12, 13, 14, 5],
+        [11, 16, 15, 6],
+        [10, 9, 8, 7]
+    ])
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    assert result == expected

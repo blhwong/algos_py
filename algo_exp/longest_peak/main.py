3612f8530449ebae7c1fe6c1dca6197741d81de6
Brandon Wong <brandon@yerdle.com>
2020-09-06 15:30:38 -0500

    Revert to original

--- algo_exp/longest_peak/main.py
+++ algo_exp/longest_peak/main.py
@@ -1,18 +1,2 @@
-def longestPeak(array):
-    ans = 0
-
-    for i in range(1, len(array) - 1):
-        prev = i - 1
-        curr = i
-        nxt = i + 1
-
-        if array[curr] > array[prev] and array[curr] > array[nxt]:
-            left, right = prev, nxt
-            while left > 0 and array[left - 1] < array[left]:
-                left -= 1
-            while right < len(array) - 1 and array[right] > array[right + 1]:
-                right += 1
-
-            ans = max(ans, right - left + 1)
-
-    return ans
+def longest_peak(array):
+    pass
--- algo_exp/longest_peak/test.py
+++ algo_exp/longest_peak/test.py
@@ -1,11 +1,11 @@
-from algo_exp.longest_peak.main import longestPeak
+from algo_exp.longest_peak.main import longest_peak
 
 
 def test_1():
-    assert longestPeak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]) == 6
+    assert longest_peak([1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]) == 6
 
 def test_2():
-    assert longestPeak([1, 3, 2]) == 3
+    assert longest_peak([1, 3, 2]) == 3
 
 def test_3():
-    assert longestPeak([1, 2, 3]) == 0
+    assert longest_peak([1, 2, 3]) == 0

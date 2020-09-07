f1c77137f792e7ea3089024eb4b64102aae9537f
Brandon Wong <brandon@yerdle.com>
2020-09-07 11:00:09 -0500

    Revert to original

--- algo_exp/validate_bst/test.py
+++ algo_exp/validate_bst/test.py
@@ -1,50 +1,43 @@
-from unittest import TestCase, main
-from algo_exp.validate_bst.main import validateBst
+from algo_exp.validate_bst.main import validate_bst
 from data_structures.tree_node import TreeNode
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        root = TreeNode(10)
-        root.left = TreeNode(5)
-        root.right = TreeNode(15)
-        root.left.left = TreeNode(2)
-        root.left.right = TreeNode(5)
-        root.left.left.left = TreeNode(1)
-        root.right.left = TreeNode(13)
-        root.right.right = TreeNode(22)
-        root.right.left.right = TreeNode(14)
+def test_1():
+    root = TreeNode(10)
+    root.left = TreeNode(5)
+    root.right = TreeNode(15)
+    root.left.left = TreeNode(2)
+    root.left.right = TreeNode(5)
+    root.left.left.left = TreeNode(1)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(22)
+    root.right.left.right = TreeNode(14)
 
-        self.assertTrue(validateBst(root))
+    assert validate_bst(root) is True
 
-    def test_2(self):
-        root = TreeNode(1)
-        root.left = TreeNode(2)
-        root.right = TreeNode(3)
+def test_2():
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
 
-        self.assertFalse(validateBst(root))
+    assert validate_bst(root) is False
 
-    def test_3(self):
-        root = TreeNode(10)
-        root.left = TreeNode(5)
-        root.right = TreeNode(15)
-        root.left.left = TreeNode(2)
-        root.left.left.left = TreeNode(1)
-        root.left.right = TreeNode(5)
-        root.right.right = TreeNode(22)
-        root.left.right.right = TreeNode(11)
+def test_3():
+    root = TreeNode(10)
+    root.left = TreeNode(5)
+    root.right = TreeNode(15)
+    root.left.left = TreeNode(2)
+    root.left.left.left = TreeNode(1)
+    root.left.right = TreeNode(5)
+    root.right.right = TreeNode(22)
+    root.left.right.right = TreeNode(11)
 
-        self.assertFalse(validateBst(root))
+    assert validate_bst(root) is False
 
-    def test_4(self):
-        root = TreeNode(10)
-        root.left = TreeNode(5)
-        root.left.right = TreeNode(10)
-        root.right = TreeNode(15)
+def test_4():
+    root = TreeNode(10)
+    root.left = TreeNode(5)
+    root.left.right = TreeNode(10)
+    root.right = TreeNode(15)
 
-        self.assertFalse(validateBst(root))
-
-
-
-if __name__ == '__main__':
-    main()
+    assert validate_bst(root) is False

157f0a4a5c992643b92016192909062faf40a0c7
Brandon Wong <brandon@yerdle.com>
2020-09-07 12:05:20 -0500

    Revert to original

--- algo_exp/coin_change_minimum/main.py
+++ algo_exp/coin_change_minimum/main.py
@@ -1,9 +1,2 @@
-def minNumberOfCoinsForChange(n, denoms):
-    dp = [float('inf')] * (n + 1)
-    dp[0] = 0
-    for coin in denoms:
-        for i in range(coin, n + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-
-    return dp[-1] if dp[-1] != float('inf') else -1
+def min_number_of_coins_for_change(n, denoms):
+    pass
--- algo_exp/coin_change_minimum/test.py
+++ algo_exp/coin_change_minimum/test.py
@@ -1,32 +1,32 @@
-from algo_exp.coin_change_minimum.main import minNumberOfCoinsForChange
+from algo_exp.coin_change_minimum.main import min_number_of_coins_for_change
 
 
 def test_1():
-    assert minNumberOfCoinsForChange(6, [1, 5]) == 2
+    assert min_number_of_coins_for_change(6, [1, 5]) == 2
 
 def test_2():
-    assert minNumberOfCoinsForChange(10, [1, 5, 10]) == 1
+    assert min_number_of_coins_for_change(10, [1, 5, 10]) == 1
 
 def test_3():
-    assert minNumberOfCoinsForChange(0, [1, 2, 3]) == 0
+    assert min_number_of_coins_for_change(0, [1, 2, 3]) == 0
 
 def test_4():
-    assert minNumberOfCoinsForChange(10, []) == -1
+    assert min_number_of_coins_for_change(10, []) == -1
 
 def test_5():
-    assert minNumberOfCoinsForChange(12, [2, 3, 7]) == 3
+    assert min_number_of_coins_for_change(12, [2, 3, 7]) == 3
 
 def test_6():
-    assert minNumberOfCoinsForChange(7, [2, 3, 4, 7]) == 1
+    assert min_number_of_coins_for_change(7, [2, 3, 4, 7]) == 1
 
 def test_7():
-    assert minNumberOfCoinsForChange(41, [1, 5, 10, 25]) == 4
+    assert min_number_of_coins_for_change(41, [1, 5, 10, 25]) == 4
 
 def test_8():
-    assert minNumberOfCoinsForChange(7, [1, 5, 10]) == 3
+    assert min_number_of_coins_for_change(7, [1, 5, 10]) == 3
 
 def test_9():
-    assert minNumberOfCoinsForChange(10, [11]) == -1
+    assert min_number_of_coins_for_change(10, [11]) == -1
 
 def test_10():
-    assert minNumberOfCoinsForChange(7, [2, 4]) == -1
+    assert min_number_of_coins_for_change(7, [2, 4]) == -1

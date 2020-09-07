feb256b9423efc0a92cad85005c7ea69277e6e5f
Brandon Wong <brandon@yerdle.com>
2020-09-07 12:31:56 -0500

    Revert to original

--- algo_exp/levenshtein_distance/test.py
+++ algo_exp/levenshtein_distance/test.py
@@ -1,5 +1,5 @@
-from algo_exp.levenshtein_distance.main import levenshteinDistance
+from algo_exp.levenshtein_distance.main import levenshtein_distance
 
 
 def test_1():
-    assert levenshteinDistance('abc', 'yabd') == 2
+    assert levenshtein_distance('abc', 'yabd') == 2

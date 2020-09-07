5b5f78e00ac851e8d198d743ffb14213ec5c3042
Brandon Wong <brandon@yerdle.com>
2020-09-07 15:13:48 -0500

    Revert to original

--- algo_exp/single_cycle_check/main.py
+++ algo_exp/single_cycle_check/main.py
@@ -1,10 +1,2 @@
-def hasSingleCycle(array):
-    i = 0
-    visited = 0
-    while visited < len(array):
-        if i == 0 and visited > 0:
-            return False
-        i = ((i + array[i]) % len(array))
-        visited += 1
-
-    return i == 0
+def has_single_cycle(array):
+    pass
--- algo_exp/single_cycle_check/test.py
+++ algo_exp/single_cycle_check/test.py
@@ -1,17 +1,17 @@
-from algo_exp.single_cycle_check.main import hasSingleCycle
+from algo_exp.single_cycle_check.main import has_single_cycle
 
 
 def test_1():
-    assert hasSingleCycle([2, 3, 1, -4, -4, 2]) is True
+    assert has_single_cycle([2, 3, 1, -4, -4, 2]) is True
 
 def test_2():
-    assert hasSingleCycle([2, 1, 1]) is False
+    assert has_single_cycle([2, 1, 1]) is False
 
 def test_3():
-    assert hasSingleCycle([2, 2, -1]) is True
+    assert has_single_cycle([2, 2, -1]) is True
 
 def test_4():
-    assert hasSingleCycle([1, 1, 0, 1, 1]) is False
+    assert has_single_cycle([1, 1, 0, 1, 1]) is False
 
 def test_5():
-    assert hasSingleCycle([1, 1, 1, 1, 2]) is False
+    assert has_single_cycle([1, 1, 1, 1, 2]) is False

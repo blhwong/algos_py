f37155e06b59bc57d877f888a2cf5e46ca456ed3
Brandon Wong <brandon@yerdle.com>
2020-09-07 12:03:29 -0500

    Add one more test

--- algo_exp/max_subset_sum_no_adjacent/test.py
+++ algo_exp/max_subset_sum_no_adjacent/test.py
@@ -3,3 +3,6 @@ from algo_exp.max_subset_sum_no_adjacent.main import max_subset_sum_no_adjacent
 
 def test_1():
     assert max_subset_sum_no_adjacent([75, 105, 120, 75, 90, 135]) == 330
+
+def test_2():
+    assert max_subset_sum_no_adjacent([]) == 0

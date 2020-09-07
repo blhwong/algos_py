580c3d976a59c0bc9bc35ff788ab72d39f590c54
Brandon Wong <brandon@yerdle.com>
2020-09-07 15:02:41 -0500

    Revert to original

--- algo_exp/kadanes_algorithm/main.py
+++ algo_exp/kadanes_algorithm/main.py
@@ -1,11 +1,2 @@
-def kadanesAlgorithm(array):
-    if not array:
-        return 0
-
-    ans = array[0]
-    curr_sum = 0
-    for num in array:
-        curr_sum = max(curr_sum + num, num)
-        ans = max(ans, curr_sum)
-
-    return ans
+def kadanes_algorithm(array):
+    pass
--- algo_exp/kadanes_algorithm/test.py
+++ algo_exp/kadanes_algorithm/test.py
@@ -1,14 +1,14 @@
-from algo_exp.kadanes_algorithm.main import kadanesAlgorithm
+from algo_exp.kadanes_algorithm.main import kadanes_algorithm
 
 
 def test_1():
-    assert kadanesAlgorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]) == 19
+    assert kadanes_algorithm([3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]) == 19
 
 def test_2():
-    assert kadanesAlgorithm([-1, -2, -3]) == -1
+    assert kadanes_algorithm([-1, -2, -3]) == -1
 
 def test_3():
-    assert kadanesAlgorithm([]) == 0
+    assert kadanes_algorithm([]) == 0
 
 def test_4():
-    assert kadanesAlgorithm([-2]) == -2
+    assert kadanes_algorithm([-2]) == -2

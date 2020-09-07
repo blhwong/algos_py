a380855e0c3bc265c9ea75c76b34f46c18a0cab7
Brandon Wong <brandon@yerdle.com>
2020-09-07 12:04:28 -0500

    Revert to original

--- algo_exp/coin_change_possibilities/main.py
+++ algo_exp/coin_change_possibilities/main.py
@@ -1,18 +1,2 @@
-"""
-   0  1  2  3  4  5  6  7  8  9  10
-1  1  1  1  1  1  1  1  1  1  1  1
-5  1  1  1  1  1  2  2  2  2  2  3
-10 1  1  1  1  1  2  2  2  2  2  4
-"""
-
-def numberOfWaysToMakeChange(n, denoms):
-    if not denoms:
-        return 0
-
-    dp = [0] * (n + 1)
-    dp[0] = 1
-    for coin in denoms:
-        for i in range(coin, n + 1):
-            dp[i] += dp[i - coin]
-
-    return dp[-1]
+def number_of_ways_to_make_change(n, denoms):
+    pass
--- algo_exp/coin_change_possibilities/test.py
+++ algo_exp/coin_change_possibilities/test.py
@@ -1,20 +1,20 @@
-from algo_exp.coin_change_possibilities.main import numberOfWaysToMakeChange
+from algo_exp.coin_change_possibilities.main import number_of_ways_to_make_change
 
 
 def test_1():
-    assert numberOfWaysToMakeChange(6, [1, 5]) == 2
+    assert number_of_ways_to_make_change(6, [1, 5]) == 2
 
 def test_2():
-    assert numberOfWaysToMakeChange(10, [1, 5, 10]) == 4
+    assert number_of_ways_to_make_change(10, [1, 5, 10]) == 4
 
 def test_3():
-    assert numberOfWaysToMakeChange(0, [1, 2, 3]) == 1
+    assert number_of_ways_to_make_change(0, [1, 2, 3]) == 1
 
 def test_4():
-    assert numberOfWaysToMakeChange(10, []) == 0
+    assert number_of_ways_to_make_change(10, []) == 0
 
 def test_5():
-    assert numberOfWaysToMakeChange(12, [2, 3, 7]) == 4
+    assert number_of_ways_to_make_change(12, [2, 3, 7]) == 4
 
 def test_6():
-    assert numberOfWaysToMakeChange(7, [2, 3, 4, 7]) == 3
+    assert number_of_ways_to_make_change(7, [2, 3, 4, 7]) == 3

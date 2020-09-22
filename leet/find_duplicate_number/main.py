5f77c2b0a3105aef7482680641037034c6cd9783
Brandon Wong <brandon@yerdle.com>
2020-09-21 22:25:03 -0500

    Implement using cyclic sort

--- leet/find_duplicate_number/main.py
+++ leet/find_duplicate_number/main.py
@@ -11,3 +11,13 @@ class Solution:
                 return num
             tracker.add(num)
 
+    def findDuplicateCyclic(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] != i + 1:
+                j = nums[i] - 1
+                if nums [i] == nums[j]:
+                    return nums[i]
+                nums[i], nums[j] = nums[j], nums[i]
+            else:
+                i += 1
--- leet/find_duplicate_number/test.py
+++ leet/find_duplicate_number/test.py
@@ -6,15 +6,19 @@ s = Solution()
 
 def test_1():
     assert s.findDuplicate([1, 3, 4, 2, 2]) == 2
+    assert s.findDuplicateCyclic([1, 3, 4, 2, 2]) == 2
 
 
 def test_2():
     assert s.findDuplicate([3, 1, 3, 4, 2]) == 3
+    assert s.findDuplicateCyclic([3, 1, 3, 4, 2]) == 3
 
 
 def test_3():
     assert s.findDuplicate([1, 1]) == 1
+    assert s.findDuplicateCyclic([1, 1]) == 1
 
 
 def test_4():
     assert s.findDuplicate([1, 1, 2]) == 1
+    assert s.findDuplicateCyclic([1, 1, 2]) == 1

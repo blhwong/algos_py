7f71c2968d263798a47718921e338c3aedbaf107
Brandon Wong <brandon@yerdle.com>
2020-09-21 22:36:59 -0500

    Implement using cycle start

--- leet/find_duplicate_number/main.py
+++ leet/find_duplicate_number/main.py
@@ -21,3 +21,30 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
             else:
                 i += 1
+
+    def findDuplicateCycleStart(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[nums[0]]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        curr = nums[nums[slow]]
+        cycle_length = 1
+
+        while curr != nums[slow]:
+            cycle_length += 1
+            curr = nums[curr]
+
+        p1, p2 = nums[0], nums[0]
+
+        while cycle_length > 0:
+            p2 = nums[p2]
+            cycle_length -= 1
+
+        while p1 != p2:
+            p1 = nums[p1]
+            p2 = nums[p2]
+
+        return p1
--- leet/find_duplicate_number/test.py
+++ leet/find_duplicate_number/test.py
@@ -7,18 +7,22 @@ s = Solution()
 def test_1():
     assert s.findDuplicate([1, 3, 4, 2, 2]) == 2
     assert s.findDuplicateCyclic([1, 3, 4, 2, 2]) == 2
+    assert s.findDuplicateCycleStart([1, 3, 4, 2, 2]) == 2
 
 
 def test_2():
     assert s.findDuplicate([3, 1, 3, 4, 2]) == 3
     assert s.findDuplicateCyclic([3, 1, 3, 4, 2]) == 3
+    assert s.findDuplicateCycleStart([3, 1, 3, 4, 2]) == 3
 
 
 def test_3():
     assert s.findDuplicate([1, 1]) == 1
     assert s.findDuplicateCyclic([1, 1]) == 1
+    assert s.findDuplicateCycleStart([1, 1]) == 1
 
 
 def test_4():
     assert s.findDuplicate([1, 1, 2]) == 1
     assert s.findDuplicateCyclic([1, 1, 2]) == 1
+    assert s.findDuplicateCycleStart([1, 1, 2]) == 1

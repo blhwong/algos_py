08ab4ac597f4c633271ea29340437e8e1f6e3b92
Brandon Wong <brandon@yerdle.com>
2020-09-21 22:09:41 -0500

    Implement find duplicate with set tracker

--- leet/find_duplicate_number/main.py
+++ leet/find_duplicate_number/main.py
@@ -2,5 +2,12 @@ from typing import List
 
 
 class Solution:
+
     def findDuplicate(self, nums: List[int]) -> int:
-        pass
+        tracker = set()
+
+        for num in nums:
+            if num in tracker:
+                return num
+            tracker.add(num)
+

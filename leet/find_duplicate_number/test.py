d33115cfc1437d93cdab5efbdef0fecf257b25e2
Brandon Wong <brandon@yerdle.com>
2020-09-21 22:08:25 -0500

    Add test for find duplicate

---
+++ leet/find_duplicate_number/test.py
@@ -0,0 +1,20 @@
+from leet.find_duplicate_number.main import Solution
+
+
+s = Solution()
+
+
+def test_1():
+    assert s.findDuplicate([1, 3, 4, 2, 2]) == 2
+
+
+def test_2():
+    assert s.findDuplicate([3, 1, 3, 4, 2]) == 3
+
+
+def test_3():
+    assert s.findDuplicate([1, 1]) == 1
+
+
+def test_4():
+    assert s.findDuplicate([1, 1, 2]) == 1

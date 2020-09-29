c371ffe18112772b312e09001578d3834f08c7aa
Brandon Wong <brandon@yerdle.com>
2020-09-29 11:48:24 -0500

    Revert to original

--- algo_exp/remove_kth_node_from_end/main.py
+++ algo_exp/remove_kth_node_from_end/main.py
@@ -4,19 +4,4 @@ When second pointer ends, first pointer will be k from the end
 """
 
 def removeKthNodeFromEnd(head, k):
-    curr, offset = head, head
-    count = 0
-    while count < k:
-        offset = offset.next
-        count += 1
-
-    if not offset:
-        head.value = head.next.value
-        head.next = head.next.next
-        return
-
-    while offset.next:
-        curr = curr.next
-        offset = offset.next
-
-    curr.next = curr.next.next
+    pass

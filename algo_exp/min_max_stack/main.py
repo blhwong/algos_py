0ad6b615665e8647fe02d40883140a608600a10d
Brandon Wong <brandon@yerdle.com>
2020-09-29 17:21:13 -0500

    Revert to original

--- algo_exp/min_max_stack/main.py
+++ algo_exp/min_max_stack/main.py
@@ -1,24 +1,16 @@
-# Feel free to add new properties and methods to the class.
 class MinMaxStack:
-    def __init__(self):
-        self.storage = []
-        self.min_max_storage = []
 
     def peek(self):
-        return self.storage[-1]
+        pass
 
     def pop(self):
-        self.min_max_storage.pop()
-        return self.storage.pop()
+        pass
 
     def push(self, number):
-        self.storage.append(number)
-        curr_min = self.getMin() if self.min_max_storage else number
-        curr_max = self.getMax() if self.min_max_storage else number
-        self.min_max_storage.append((min(curr_min, number), max(curr_max, number)))
+        pass
 
     def getMin(self):
-        return self.min_max_storage[-1][0]
+        pass
 
     def getMax(self):
-        return self.min_max_storage[-1][1]
+        pass

8f51db12827a336146aaf0717686e132d40967cf
Brandon Wong <brandon@yerdle.com>
2020-09-29 18:01:56 -0500

    Cleanup

--- algo_exp/balanced_brackets/test.py
+++ algo_exp/balanced_brackets/test.py
@@ -4,8 +4,10 @@ from algo_exp.balanced_brackets.main import balanced_brackets
 def test_1():
     assert balanced_brackets('([])(){}(())()()') is True
 
+
 def test_2():
     assert balanced_brackets('[(])') is False
+
 
 def test_3():
     assert balanced_brackets('(a)') is True

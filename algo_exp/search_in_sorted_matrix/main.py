0a16e8ee1c9b04ae2a023306d4e003a11478101d
Brandon Wong <brandon@yerdle.com>
2020-09-29 13:59:31 -0500

    Revert to original

--- algo_exp/search_in_sorted_matrix/main.py
+++ algo_exp/search_in_sorted_matrix/main.py
@@ -1,14 +1,2 @@
 def searchInSortedMatrix(matrix, target):
-    row, col = 0, len(matrix[0]) - 1
-    ans = [-1, -1]
-
-    while row >= 0 and col >= 0:
-        val = matrix[row][col]
-        if val == target:
-            return [row, col]
-        elif val < target:
-            row += 1
-        else:
-            col -= 1
-
-    return ans
+    pass

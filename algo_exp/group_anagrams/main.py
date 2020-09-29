488faea5a2c7dce89c442be45a2ec130bd6ba95c
Brandon Wong <brandon@yerdle.com>
2020-09-29 18:03:43 -0500

    Revert to original

--- algo_exp/group_anagrams/main.py
+++ algo_exp/group_anagrams/main.py
@@ -1,10 +1,2 @@
-def groupAnagrams(words):
-    tracker = {}
-    for word in words:
-        key = ''.join(sorted([char for char in word]))
-        if key not in tracker:
-            tracker[key] = []
-
-        tracker[key].append(word)
-
-    return list(tracker.values())
+def group_anagrams(words):
+    pass
--- algo_exp/group_anagrams/test.py
+++ algo_exp/group_anagrams/test.py
@@ -1,15 +1,16 @@
 from unittest import TestCase, main
-from algo_exp.group_anagrams.main import groupAnagrams
+from algo_exp.group_anagrams.main import group_anagrams
 
 
 class TestSuite(TestCase):
+
     def test_1(self):
         expected = [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
-        self.assertCountEqual(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']), expected)
+        self.assertCountEqual(group_anagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']), expected)
 
     def test_2(self):
         expected = [['yo', 'oy',], ['flop', 'olfp'], ['act', 'tac', 'cat']]
-        self.assertCountEqual(groupAnagrams(['yo', 'act', 'flop', 'tac', 'cat', 'oy', 'olfp']), expected)
+        self.assertCountEqual(group_anagrams(['yo', 'act', 'flop', 'tac', 'cat', 'oy', 'olfp']), expected)
 
 
 if __name__ == '__main__':

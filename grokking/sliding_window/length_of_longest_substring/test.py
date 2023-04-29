a415d6070826687a33b76915e32633154feb2737
Brandon Wong <b.lh.wong@gmail.com>
2023-04-29 09:01:15 -0500

    Assert tests

--- grokking/sliding_window/length_of_longest_substring/test.py
+++ grokking/sliding_window/length_of_longest_substring/test.py
@@ -2,13 +2,13 @@ from grokking.sliding_window.length_of_longest_substring.main import length_of_l
 
 
 def test_1():
-    length_of_longest_substring('aabccbb', 2) == 5
+    assert length_of_longest_substring('aabccbb', 2) == 5
 
 def test_2():
-    length_of_longest_substring('abbcb', 1) == 4
+    assert length_of_longest_substring('abbcb', 1) == 4
 
 def test_3():
-    length_of_longest_substring('abccde', 1) == 3
+    assert length_of_longest_substring('abccde', 1) == 3
 
 def test_4():
-    length_of_longest_substring('abbcb', 2) == 5
+    assert length_of_longest_substring('abbcb', 2) == 5

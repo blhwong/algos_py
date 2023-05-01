5d691963f41762c856a7e2b577bd57a457a21b7c
Brandon Wong <b.lh.wong@gmail.com>
2023-05-01 12:00:36 -0500

    Change to pytest assertion

--- grokking/cyclic_sort/find_missing_numbers/test.py
+++ grokking/cyclic_sort/find_missing_numbers/test.py
@@ -1,17 +1,11 @@
-from unittest import TestCase, main
 from grokking.cyclic_sort.find_missing_numbers.main import find_missing_numbers
 
 
-class TestSuite(TestCase):
-    def test_1(self):
-        self.assertListEqual(find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]), [4, 6, 7])
+def test_1():
+    assert find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]) == [4, 6, 7]
 
-    def test_2(self):
-        self.assertListEqual(find_missing_numbers([2, 4, 1, 2]), [3])
+def test_2():
+    assert find_missing_numbers([2, 4, 1, 2]) == [3]
 
-    def test_3(self):
-        self.assertListEqual(find_missing_numbers([2, 3, 2, 1]), [4])
-
-if __name__ == '__main__':
-
-    main()
+def test_3():
+    assert find_missing_numbers([2, 3, 2, 1]) == [4]

6e688cbe97bead7a55e18a9ebe3a02ecb4e91dcf
Brandon Wong <b.lh.wong@gmail.com>
2023-05-01 11:04:05 -0500

    Use native eq instead of compare since better debug msg

--- data_structures/interval.py
+++ data_structures/interval.py
@@ -6,17 +6,8 @@ class Interval:
     def __repr__(self):
         return f'[{self.start}, {self.end}]'
 
+    def __eq__(self, interval):
+        return self.start == interval.start and self.end == interval.end
+
     def print_interval(self):
         print('[' + str(self.start) + ', ' + str(self.end) + ']', end='')
-
-    @staticmethod
-    def compare(intervals1, intervals2):
-        if len(intervals1) != len(intervals2):
-            return False
-        for idx, val in enumerate(intervals1):
-            start1, end1 = intervals1[idx].start, intervals1[idx].end
-            start2, end2 = intervals2[idx].start, intervals2[idx].end
-            if start1 != start2 or end1 != end2:
-                return False
-
-        return True
--- grokking/merge_intervals/merge_intervals/test.py
+++ grokking/merge_intervals/merge_intervals/test.py
@@ -6,18 +6,18 @@ def test_1():
     intervals = [Interval(1, 4), Interval(2, 5), Interval(7, 9)]
     merged = merge(intervals)
     expected = [Interval(1, 5), Interval(7, 9)]
-    assert Interval.compare(merged, expected)
+    assert merged == expected
 
 
 def test_2():
     intervals = [Interval(6, 7), Interval(2, 4), Interval(5, 9)]
     merged = merge(intervals)
     expected = [Interval(2, 4), Interval(5, 9)]
-    assert Interval.compare(merged, expected)
+    assert merged == expected
 
 
 def test_3():
     intervals = [Interval(1, 4), Interval(2, 6), Interval(3, 5)]
     merged = merge(intervals)
     expected = [Interval(1, 6)]
-    assert Interval.compare(merged, expected)
+    assert merged == expected
